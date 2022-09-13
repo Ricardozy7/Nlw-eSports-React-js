540950579f3de0992f739960076ef2d6e3e5b4ad
Ricard1nho <diasricardo204@gmail.com>
2022-09-13 14:42:10 -0300

    High speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import "./styles/main.css"
+
+import { MagnifyingGlassPlus } from "phosphor-react"
+
+import logImg from "./assets/Logo-nlw-esports.svg"
+
 function App() {
 
   return (
-    <h1>hello world</h1>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="bg-nlw-gradient text-transparent bg-clip-text">duo</span> está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16 ">
+        {
+          [1, 2, 3, 4, 5, 6].map((e) => (
+            <a href="" className="relative rounded-lg overflow-hidden ">
+              <img src={`/image ${e}.png`} alt="" />
+              <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">League of leagends</strong>
+                <span className="text-zinc-300 text-sm block">4 anúncios</span>
+              </div>
+            </a>
+          ))
+        }
+      </div>
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between">
+          <div>
+            <strong className="tex-2xl text-white font-black block">Não encotrou o seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
